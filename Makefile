ad578bfc7f37d6615bc799ff2c56779410cb4918
Kyohei Ito <je.suis.kyohei+github@gmail.com>
2020-01-29 18:43:01 +0900

    Fix Packages

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 gen:
-	swift build --product protoc-gen-swiftgrpc-client -c release
-	cp .build/release/protoc-gen-swiftgrpc-client .
+	swift build --product protoc-gen-grpc-client-swift -c release
+	cp .build/release/protoc-gen-grpc-client-swift .
 
 all:
 	swift build
 	cp .build/debug/protoc-gen-swift .
-	cp .build/debug/protoc-gen-swiftgrpc-client .
+	cp .build/debug/protoc-gen-grpc-client-swift .
 
 project:
 	swift package generate-xcodeproj
@@ -13,6 +13,6 @@ project:
 clean:
 	rm -rf Packages
 	rm -rf .build build DerivedData
-	rm -rf SwiftGRPCClient.xcodeproj
+	rm -rf GRPCClient.xcodeproj
 	rm -rf Package.pins Package.resolved
-	rm -rf protoc-gen-swift protoc-gen-swiftgrpc-client
+	rm -rf protoc-gen-swift protoc-gen-grpc-client-swift
--- Package.swift
+++ Package.swift
@@ -4,19 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGRPCClient",
+    name: "GRPCClient",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v12),
+        .tvOS(.v12)
+    ],
     products: [
-        .library(name: "SwiftGRPCClient", targets: ["SwiftGRPCClient"])
+        .library(name: "GRPCClient", targets: ["GRPCClient"]),
+        .executable(name: "protoc-gen-grpc-client-swift", targets: ["protoc-gen-grpc-client-swift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.5.0")),
-        .package(url: "https://github.com/grpc/grpc-swift.git", from: "0.9.1")
+        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.7.0")),
+        .package(url: "https://github.com/grpc/grpc-swift.git", .exact("1.0.0-alpha.9"))
     ],
     targets: [
-        .target(name: "SwiftGRPCClient",
-                dependencies: ["SwiftGRPC"]),
-        .target(name: "protoc-gen-swiftgrpc-client",
+        .target(name: "GRPCClient",
+                dependencies: ["GRPC"]),
+        .target(name: "protoc-gen-grpc-client-swift",
                 dependencies: [
                     "SwiftProtobufPluginLibrary",
                     "protoc-gen-swift"])
-    ])
+    ]
+)
